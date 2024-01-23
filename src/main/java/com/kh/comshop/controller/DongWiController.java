99781b232b99acf14a2d671774d649b1cbb8da82
hyunsu <hansu0809@naver.com>
2024-01-23 10:04:43 +0900

    project rebuild

---
+++ src/main/java/com/kh/comshop/controller/DongWiController.java
@@ -0,0 +1,13 @@
+package com.kh.comshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DongWiController {
+	
+	@GetMapping("/main")
+	public void main() {
+		
+	}
+}
--- src/main/java/com/kh/comshop/controller/HomeController.java
+++ src/main/java/com/kh/comshop/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.kh.comshop;
+package com.kh.comshop.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -11,17 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -32,6 +26,11 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		System.out.println("a");
+		System.out.println("b");
+		System.out.println("c");
+		System.out.println("ㅇ");
 		
 		return "home";
 	}
