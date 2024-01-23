856dbe72fa94e85c47edafd9f3de476d8ef8cb2e
dongdongyeong <154576465+dongdongyeong@users.noreply.github.com>
2024-01-23 12:34:34 +0900

    test01 파일 교체
    파일 교체

---
+++ src/main/java/com/kh/test/controller/DongyeongController.java
@@ -0,0 +1,31 @@
+package com.kh.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customerCenter")
+public class DongyeongController {
+	
+	@GetMapping("/FAQ")
+	public void FAQ() {
+		
+	}
+	@GetMapping("/notice")
+	public void notice() {
+		
+	}
+	@GetMapping("/inquiry")
+	public void inquiry() {
+		
+	}
+	@GetMapping("/question")
+	public void question() {
+		
+	}
+	@GetMapping("/login")
+	public void login() {
+		
+	}
+}
